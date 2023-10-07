2630a1b7fbf57cab2b998fdf727bc81a2e3e53d7
javierprogrameitor <javierprogrameitor@gmail.com>
2023-10-07 06:03:32 +0200

    Registro  terminado
    He cambiado el proyecto como me indican en la tarea

--- js/script.js
+++ js/script.js
@@ -2,37 +2,36 @@ let form = document.getElementById("login-form");
 let inputUser = document.getElementById("user");
 let inputPass = document.getElementById("password");
 let inputRepitPass = document.getElementById("repitpass");
-let inputEmail = document.getElementById("email");
+let inputTelefono = document.getElementById("telefono");
 let submitButton = document.getElementById("button");
 
 let nameValid = false;
 let passwordValid = false;
-let repitValid = false;
-let emailValid = false;
+let telefonoValid = false;
 let regexName = /^[a-zA-Z ]+$/;
 let regexPassword = /^[a-zA-Z0-9]{6,}$/;
-let regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+let regexTelefono = /^\d{9}$/;
+
 
 const USERNAME_INVALID = "Nombre con sólo letras y espacios";
 const PASSWORD_INVALID = "Contraseña con 6 o más carácteres";
 const REPITPASS_INVALID = "Repite la contraseña no coincide";
-const EMAIL_INVALID = "Email incorrecto";
+const TLF_INVALID = "Telefono incorrecto";
 
 
 
 inputUser.addEventListener("blur", validName);
 inputPass.addEventListener("blur", validPassword);
 inputRepitPass.addEventListener("blur", validRepitPass);
-inputEmail.addEventListener("blur", validEmail);
+inputTelefono.addEventListener("blur", validTelefono);
 
 function check() {
-    if (nameValid && passwordValid && repitValid && emailValid ) {
+    if (nameValid && passwordValid && telefonoValid) {
       submitButton.classList.remove("notAvailable");
     } else {
       submitButton.classList = "notAvailable";
     }
   }
-
 
   function validName() {
     nameValid = regexName.test(inputUser.value);
@@ -62,40 +61,35 @@ function validPassword() {
     check();
 }
 
-
 function validRepitPass() {
 
-  if(inputRepitPass == inputPass){
+  let pass = document.getElementById("password").value;
+  let rept = document.getElementById("repitpass").value;
 
-    if(!repitValid){
+  if(pass === rept){
+    inputRepitPass.className = "success";
+
+    inputRepitPass.parentNode.getElementsByTagName("small")[0].innerHTML = "";
+    } else {
+      inputRepitPass.className = "error";
       // Obtenemos la etiqueta <small> del div al que pertenece el input
       inputRepitPass.parentNode.getElementsByTagName("small")[0].innerHTML = REPITPASS_INVALID;
-    } else {
-      inputRepitPass.parentNode.getElementsByTagName("small")[0].innerHTML = "";
     }
-
-    check();
+  check();
   }
 
-  
+function validTelefono() {
+  telefonoValid = regexTelefono.test(inputTelefono.value);
+  inputTelefono.className = telefonoValid ? "success" : "error";
 
+  if (!telefonoValid) {
+    // Obtenemos la etiqueta <small> del div al que pertenece el input
+    inputTelefono.parentNode.getElementsByTagName("small")[0].innerHTML = TLF_INVALID;
+  } else {
+      inputTelefono.parentNode.getElementsByTagName("small")[0].innerHTML = "";
+  }
+  check();
 }
-
-function validEmail() {
-    emailValid = regexEmail.test(inputEmail.value);
-    inputEmail.className = emailValid ? "success" : "error";
-  
-    if (!emailValid) {
-      // Obtenemos la etiqueta <small> del div al que pertenece el input
-      inputEmail.parentNode.getElementsByTagName("small")[0].innerHTML = EMAIL_INVALID;
-    } else {
-        inputEmail.parentNode.getElementsByTagName("small")[0].innerHTML = "";
-    }
-
-    check();
-}
-
-
 
 form.addEventListener('submit', event => {
   /*
